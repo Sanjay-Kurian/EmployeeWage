25eae354d16410c7c60a3bd8eb64a4ad284da2a4
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-21 21:41:07 +0530

    Added:Part time employees and wage

--- EmployeeWage/src/HelloMessage.java
+++ EmployeeWage/src/HelloMessage.java
@@ -1,9 +1,10 @@
 
 public class HelloMessage {
 	public static void main(String[] args) {
-		double empCheck = Math.floor((Math.random()*10)%2);
+		double empCheck = Math.floor((Math.random()*10)%3);
 		int WAGE_PER_HOUR = 20;
 		int FULL_DAY_HOUR = 8;
+		int PART_TIME_HOUR = 4;
 		int wage;
 		if (empCheck == 1)
 		{
@@ -11,10 +12,16 @@ public class HelloMessage {
 			System.out.println("Employee is present");
 			System.out.println("The wage is "+wage);
 		}
-		else
+		else if(empCheck == 0)
 		{
 			wage=0;
 			System.out.println("Employee is absent");
+			System.out.println("The wage is "+wage);
+		}
+		else
+		{
+			wage = WAGE_PER_HOUR * PART_TIME_HOUR;
+			System.out.println("Employee is present");
 			System.out.println("The wage is "+wage);
 		}
 		
