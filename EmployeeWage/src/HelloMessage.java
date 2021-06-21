648c861bd176be520f924edd0cee476d55798ff2
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-21 21:10:40 +0530

    Added:Determined whether the employee is present or absent

--- EmployeeWage/src/HelloMessage.java
+++ EmployeeWage/src/HelloMessage.java
@@ -1,6 +1,14 @@
 
 public class HelloMessage {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		double empCheck = Math.floor((Math.random()*10)%2);
+		if (empCheck == 1)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }
